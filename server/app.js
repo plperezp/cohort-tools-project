0e50a6a8f70aa1c8fed38e2d850cf76dc303d0b6
Pedro Perez <plperezpiris@gmail.com>
2024-10-09 09:28:03 +0200

    Co-authored-by: Clara Seijo <clasei@proton.me>

--- server/app.js
+++ server/app.js
@@ -18,7 +18,6 @@ const app = express();
 
 //MONGOOSE
 //mongodb://127.0.0.1 esto es la ip por defecto
-
 mongoose.connect("mongodb://127.0.0.1:27017/cohorts-tools-api")
 .then(x=>console.log(`Connected to Database: "${x.connections[0].name}`))
 .catch(err => console.error("Error connecting to MongoDB", err))
