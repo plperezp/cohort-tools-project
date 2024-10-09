7b762890e6496a7713f75712b28e77bf121a8f25
Pedro Perez <plperezpiris@gmail.com>
2024-10-09 19:03:41 +0200

    almost day3

--- server/app.js
+++ server/app.js
@@ -4,8 +4,7 @@ const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cors = require('cors')
 const mongoose = require("mongoose")
-const Cohorts = require("./models/cohorts")
-const Students = require("./models/student")
+
 
 
 // STATIC DATA
@@ -18,7 +17,7 @@ const app = express();
 
 //MONGOOSE
 //mongodb://127.0.0.1 esto es la ip por defecto
-mongoose.connect("mongodb://127.0.0.1:27017/cohorts-tools-api")
+mongoose.connect("mongodb://127.0.0.1:27017/cohort-tools-api")
 .then(x=>console.log(`Connected to Database: "${x.connections[0].name}`))
 .catch(err => console.error("Error connecting to MongoDB", err))
 
@@ -49,16 +48,188 @@ app.get("/docs", (req, res) => {
 });
 
 
-const cohorts = require("./cohorts.json")
+//ROUTES
 
-app.get("/api/cohorts", (req, res) =>{
-  res.json(cohorts)
+const Student = require("./models/student.model")
+const Cohort = require("./models/cohort.model")
+
+//ROUTES STUDENT
+
+app.post("/api/students", async (req, res) =>{
+  try {
+    const response = await Student.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      linkedinUrl: req.body.linkedinUrl,
+      languages:req.body.languages,
+      program:req.body.program,
+      background:req.body.background,
+      image: req.body.image,
+      projects: req.body.projects
+    })
+    res.status(201).json(response)
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"})
+  }
+
 })
 
-const students = require("./students.json")
-app.get("/api/students", (req, res) =>{
-  res.json(students)
+app.get("/api/students", async (req, res)=>{
+  try {
+    const response = await Student.find()
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "student ID not found"})
+  }
 })
+
+
+app.get("/api/students/cohort/:cohortId", async (req, res) =>{
+  try {
+    const response = await Student.find({ cohort: req.params.cohortId }) // possible populate
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "Internal Server Error"})
+  }
+})
+
+
+// GET a specific student by id
+app.get("/api/students/:studentId", async (req, res) => {
+  try {
+    const response = await Student.findById( req.params.studentId )
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "error fetching student by id"})
+  }
+})
+
+
+// UPDATE a specific student by id
+app.put("/api/students/:studentId", async (req, res) => {
+  try {
+    const response = await Student.findByIdAndUpdate( req.params.studentId, {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      linkedinUrl: req.body.linkedinUrl,
+      languages:req.body.languages,
+      program:req.body.program,
+      background:req.body.background,
+      image: req.body.image,
+      projects: req.body.projects 
+    }, { new: true })
+
+    res.status(200).json(response)
+    
+    
+  } catch (error) {
+    res.status(500).json({message: "error updating student by id"})
+  }
+})
+
+// DELETE a specific student by id
+app.delete("/api/students/:studentsId", async (req, res) => {
+
+  try {
+    const response = await Student.findByIdAndDelete( req.params.studentId )
+    res.status(204).json({message: "Deleted student"})
+    
+  } catch (error) {
+    res.status(500).json({message: "error deleting student by id"})
+  }
+})
+
+// COHORT ROUTES
+//! POST aka create a new cohort
+app.post("/api/cohorts", async (req, res) => {
+  
+  try {
+
+    const response = await Cohort.create({ 
+
+      cohortSlug: req.body.cohortSlug,
+      cohortName: req.body.cohortName,
+      program: req.body.program,
+      format: req.body.format,
+      campus: req.body.campus,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      inProgress: req.body.inProgress,
+      programManager: req.body.programManager,
+      leadTeacher: req.body.leadTeacher,
+      totalHours: req.body.totalHours
+   }) 
+   
+   res.status(201).json(response)
+   console.log(response)
+  }   catch (error) {
+    res.status(500).json({message: "error creating cohort"})
+    
+  }
+})
+
+// GET all the cohorts
+app.get("/api/cohorts", async (req, res) => {
+  try {
+    const response = await Cohort.find()
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "error fetching cohorts"})
+  }
+})
+
+
+//GET COHORT ID
+app.get("/api/cohorts/:cohortId", async (req, res)=>{
+  try {
+
+    const response = await Cohort.findById(req.params.cohortId)
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "error finding ID cohorts"})
+  }
+})
+
+
+// // PUT aka updates a specific cohort
+app.put("/api/cohorts/:cohortId", async (req, res) =>{
+  try {
+    const response = await Cohort.findByIdAndUpdate(req.params.cohortId,{     
+      cohortSlug: req.body.cohortSlug,
+      cohortName: req.body.cohortName,
+      program: req.body.program,
+      format: req.body.format,
+      campus: req.body.campus,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      inProgress: req.body.inProgress,
+      programManager: req.body.programManager,
+      leadTeacher: req.body.leadTeacher,
+      totalHours: req.body.totalHours
+    },{new: true})
+     res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({message: "error updating ID cohorts"})
+  }
+})
+
+//DELETE COHORTS
+
+app.delete("/api/cohorts/:cohortId", async (req, res) =>{
+  try {
+
+    const response = await Cohort.findByIdAndDelete(req.params.cohortId)
+    res.status(204).json({message: "Deleted cohort"})
+    
+  } catch (error) {
+    res.status(500).json({message: "error deleting cohort by id"})
+  }
+})
+
 
 // START SERVER
 app.listen(PORT, () => {
--- server/models/student.model.js
+++ server/models/student.model.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema;
-const {Types} = mongoose;
-const {ObjectId} = Types;
 
-const studentSchema = new Schema({
+
+
+const studentSchema = new mongoose.Schema({
 
   firstName: String,
   lastName: String,
@@ -14,6 +13,10 @@ const studentSchema = new Schema({
   program:{type: String, enum:["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"]},
   background:{type: String, default:"Empty"},
   image: {type: String, default: "https://i.imgur.com/r8bo8u7.png"},
-  cohort: {type: ObjectId, ref: "Cohort"},
+  cohort: {type: mongoose.Schema.Types.ObjectId, ref: "Cohort"},
   projects: Array
   })
+
+  const Student = mongoose.model("Student", studentSchema)
+
+  module.exports = Student
